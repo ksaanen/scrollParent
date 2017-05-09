3dc1e77329d6fb78714691dfadc81716573999bb
Kevin Saanen <kevinsaanen@gmail.com>
2017-05-09 13:27:54 +0200

    initial version

---
+++ scrollparent.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+function findScrollParentOf(element:string) {
+  let el = document.querySelector(element);
+  let parent = el.parentElement;
+  while (parent !== document.body) {
+    let iteration = parent;
+    if (iteration.scrollHeight > iteration.offsetHeight) return iteration;
+    parent = iteration.parentElement;
+  }
+  return document.body;
+}
